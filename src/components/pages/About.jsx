5fb5e55f8ae8a77b923d4489438f5b665aa65424
Igor Stojadinovic <Rogy.vs@gmail.com>
2021-07-09 20:41:12 +0200

    first commit

---
+++ src/components/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="about">
+      <h1>About This App</h1>
+      <p>App to Search Github users</p>
+    </div>
+  );
+};
+
+export default About;
